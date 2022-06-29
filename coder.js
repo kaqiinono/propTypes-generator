228121d79e19c459026ef7d709b88b258b3b9057
songmeinuo <songmeinuo@jd.com>
2022-06-29 16:57:15 +0800

    refactor: 去除无用代码

--- coder.js
+++
@@ -1,6 +0,0 @@
-export * from "./Button";
-export xxx from "./Button"
-
-export function Buxxx(){
-  return <div>xxx</div>
-}
--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const actionHelper = require('./utils/actionHelper');
 const { validFile } = require('./actions/finalExport');
 
 async function generate(srcCode, componentName) {
-  debugger
   if (!srcCode) {
     return;
   }
@@ -25,22 +24,4 @@ async function generate(srcCode, componentName) {
 }
 
 exports.generate = generate;
-exports.flowAst = astHelper.flowAst;
-
-
-// const { files } = require('./files.json');
-// const { findExports } = require('./actions/finalExport');
-// const result = [];
-// findExports(files, '/index.js', result);
-// console.log(result);
-// result.forEach(r => {
-//   const ret = {};
-//
-//   generate(validFile(files, r.filePath)?.code, r.fileName).then(d => fs.writeFileSync('./RESULT.json', JSON.stringify(d)))
-// });
-
-
-const path = require('path');
-const fs = require('fs');
-const srcCode = fs.readFileSync(path.resolve(__dirname,'./Button.jsx'));
-generate(srcCode,'Button')
+exports.validFile = validFile;
